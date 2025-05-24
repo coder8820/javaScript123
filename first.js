dcf14d25609f24349eade9df01d12fd11214ba75
coder8820 <kumail112113@gmail.com>
2025-05-24 04:42:26 -0700

    updated with function

--- first.js
+++ first.js
@@ -1,2 +1,9 @@
 // this is my first javaScript file
 console.log("Hello, World!");
+
+// JavaScript function to add two numbers
+function addNumbers(a, b) {
+    return a + b;
+}
+// Example usage of the addNumbers function
+console.log("Sum of 5 and 10 is: " + addNumbers(5, 10));
