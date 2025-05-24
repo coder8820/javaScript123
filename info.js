1bb057ecfa32cc566ea64393fcd8e1d69d9e6c59
coder8820 <kumail112113@gmail.com>
2025-05-24 04:55:04 -0700

    new files are added

---
+++ info.js
@@ -0,0 +1,8 @@
+// What is the use of JavaScript?
+// JavaScript is a versatile programming language primarily used for web development.
+//  It allows developers to create interactive and dynamic web pages, handle events, 
+// manipulate the Document Object Model (DOM), and communicate with servers through AJAX.
+//  JavaScript can also be used for server-side programming with environments like Node.js,
+//  enabling full-stack development. Additionally, it supports functional and
+//  object-oriented programming paradigms, making it suitable for a wide range of 
+// applications beyond just web development.
